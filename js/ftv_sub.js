36e7e253a1e72ce66d43111cbc59792a6cf32243
Developer01 <125529180+front-developer01@users.noreply.github.com>
2023-02-24 08:00:44 +0000

    서브제작

--- js/ftv_sub.js
+++ js/ftv_sub.js
@@ -17,3 +17,19 @@ var swiper = new Swiper(".push", {
         disableOnInteraction: false
     },
   });
+
+  let tabBt = document.querySelectorAll('.tabBt > button'); // 탭버튼
+  Array.from(tabBt).forEach((eachButton, index) => {
+    eachButton.addEventListener('click', function () {
+      let tabBoxs = document.querySelectorAll('.tabContent .tab'); // 탭박스
+      for (var i = 0; i < tabBoxs.length; i++) {
+        tabBoxs[i].classList.remove("on");
+        tabBt[i].classList.remove("on");
+        // 모든 버튼과 박스에서 on제거
+      }
+      tabBoxs[index].classList.add("on");
+      // 내가 누른 탭버튼과 인덱스번호가 같은 탭박스에 class on추가
+      this.classList.add("on");
+    });
+  });
+  tabBt[0].click(); // 첫번째 탭 켜기
